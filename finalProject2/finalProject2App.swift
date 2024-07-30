6bd6f8546ffbf29064201be0c4d64475d4cc9867
Najma <najma.ib13@gmail.com>
2024-07-30 11:28:39 +0100

    Initial Commit

---
+++ finalProject2/finalProject2App.swift
@@ -0,0 +1,17 @@
+//
+//  finalProject2App.swift
+//  finalProject2
+//
+//  Created by Scholar on 30/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct finalProject2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
